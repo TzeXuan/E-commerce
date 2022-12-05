137e32f7c5d3b6434988caf78a5b2a62d1a10ec7
TzeXuan <70616530+TzeXuan@users.noreply.github.com>
2022-12-06 00:11:31 +0800

    create venv

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
+import papermill as pm
 
 app = Flask(__name__)
 
-@app.route('/searchProduct')
+@app.route('/')
 def index():
-    return redirect(url_for('searchProduct'))
+    return redirect(url_for('search'))
 
-@app.route('/searchProduct', method = ['POST'])
+@app.route('/search')
+def search():
+    return render_template('searchProduct.html')
+
+@app.route('/searchProduct', methods = ['POST'])
 def searchProduct():
-    # user = request.form['productSKU']
-    # print(user)
-    return render_template('demand.html')
+    productSKU = request.form['productSKU']
+    print(productSKU)
+
+    pm.execute_notebook('C:\\Users\\xuan4\\Desktop\\FYP\\Dreamshop\\Untitled Folder\\Potential Customer List.ipynb',
+    'C:\\Users\\xuan4\\Desktop\\FYP\\Dreamshop\\Untitled Folder\\Potential Customer List Output.ipynb',
+    parameters={'productSKU': productSKU})
+    
+    return render_template('demand.html', productSKU=productSKU)
