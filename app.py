5e809ebdc94771f7459a4d1478b97606609fccee
TzeXuan <70616530+TzeXuan@users.noreply.github.com>
2022-12-08 01:02:06 +0800

    complete graph

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 import papermill as pm
 import pandas as pd
+import json
+import plotly 
+import plotly.express as px
 
 app = Flask(__name__)
 
@@ -14,6 +17,7 @@ def search():
 
 @app.route('/searchProduct', methods = ['POST'])
 def searchProduct():
+
     # productSKU
     productSKUInput = request.form['productSKU']
     print(productSKUInput)
@@ -21,6 +25,16 @@ def searchProduct():
     pm.execute_notebook('C:\\Users\\xuan4\\Desktop\\FYP\\Dreamshop\\Version 3\\Potential Customer List.ipynb',
     'C:\\Users\\xuan4\\Desktop\\FYP\\Dreamshop\\Version 3\\Potential Customer List Output.ipynb',
     parameters={'productSKU': productSKUInput})
+  
+    # vizualizeGraph
+    pm.execute_notebook('C:\\Users\\xuan4\\Desktop\\FYP\\Dreamshop\\Version 3\\Graph Visualization based product SKU.ipynb',
+                    'C:\\Users\\xuan4\\Desktop\\FYP\\Dreamshop\\Version 3\\Graph Visualization based product SKU Output.ipynb',
+                    parameters={'productSKU': productSKUInput})
+
+    df = pd.read_excel('C:\\Users\\xuan4\\Desktop\\FYP\\Dreamshop\\Version 3\\Graph Data of Product Quantity Sold.xlsx')
+    fig = px.line(df, x=df['Month-Year'], y=df['Quantity'])
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    print(graphJSON)
     
     # productQty
     datasetQty = pd.read_excel('productQuantity.xlsx')
@@ -30,4 +44,5 @@ def searchProduct():
     #potentialCustomer
     datasetCustomer = pd.read_excel('Potential Customer List.xlsx')
 
-    return render_template('demand.html', productSKU=productSKUInput, productQty=qtyRow[0], datasetCustomer=datasetCustomer)
+    return render_template('demand.html', productSKU=productSKUInput, productQty=qtyRow[0], datasetCustomer=datasetCustomer,
+    graphJSON=graphJSON)
